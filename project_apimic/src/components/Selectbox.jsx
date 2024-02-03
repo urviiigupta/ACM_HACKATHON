26b84fd529f43661093258fc820d1dd87ffaa9ca
Urvi Gupta <121297598+urviiigupta@users.noreply.github.com>
2024-02-03 21:17:31 +0530

    init

--- project_apimic/src/components/Selectbox.jsx
+++ project_apimic/src/components/Selectbox.jsx
@@ -1,24 +1,38 @@
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import styles from './Selectbox.module.css'
+
 const Selectbox=(props)=>{
-    let [langsearch,setlangsearch]=useState("")
+    let [inputValue, setInputValue] = useState("");
+    let [selectedLanguage, setSelectedLanguage] = useState("");
     const [id] = useState(uuidv4());  // generate a unique id for the datalist
 
-    const handlelangsearch=(event)=>{
-        setlangsearch(event.target.value)
+    const handleInputChange=(event)=>{
+        setInputValue(event.target.value);
+    }
+
+    const handleSelectionChange=(event)=>{
+        const selectedLangCode = event.target.value;
+        const selectedLanguage = props.languages.find(language => language.langcode === selectedLangCode);
+        setSelectedLanguage(selectedLanguage ? selectedLanguage.langname : "");
         props.handleSelectChange(event);
     }
+
+    useEffect(() => {
+        setInputValue(selectedLanguage);
+    }, [selectedLanguage]);
 
     return(
         <>
         <h1 className="inlineheading">  </h1>
-        <input className={styles.selectboxcss} list={id} value={langsearch.langname}  onChange={handlelangsearch} />
+        {console.log(inputValue)}
+        <input className={styles.selectboxcss} list={id} value={inputValue} onChange={handleInputChange} onBlur={handleSelectionChange} />
         <datalist id={id}>
            {props.languages.map((language) => {
                 return <option key={language.langcode} value={language.langcode}>{language.langname}</option>;
             })}
         </datalist>
+       
         </>
     )
 }
