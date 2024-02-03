884c2771ea0882297a5ac96f871e199ac1029d72
Urvi Gupta <121297598+urviiigupta@users.noreply.github.com>
2024-02-03 18:03:52 +0530

    Update Selectbox.jsx

--- project_apimic/src/components/Selectbox.jsx
+++ project_apimic/src/components/Selectbox.jsx
@@ -1,25 +1,21 @@
-import { useState } from "react";
-import styles from './Selectbox.module.css';
-
-
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import styles from './Selectbox.module.css'
 const Selectbox=(props)=>{
     let [langsearch,setlangsearch]=useState("")
-    
+    const [id] = useState(uuidv4());  // generate a unique id for the datalist
+
     const handlelangsearch=(event)=>{
         setlangsearch(event.target.value)
         props.handleSelectChange(event);
     }
 
-    const filteredLanguages = props.languages.filter(language => 
-        language.langname.toLowerCase().startsWith(langsearch.toLowerCase())
-    );
-
     return(
         <>
-        <input list="languages" value={langsearch} onChange={handlelangsearch} />
-        <datalist id="languages">
-           {filteredLanguages.map((language) => {
-                return <option  className={styles.selectboxcss} key={language.langcode} value={language.langname} >{language.langname}</option>;
+        <input list={id} value={langsearch} onChange={handlelangsearch} />
+        <datalist id={id}>
+           {props.languages.map((language) => {
+                return <option key={language.langcode} className={styles.selectboxcss} value={language.langcode}>{language.langname}</option>;
             })}
         </datalist>
         </>
